34e96a0d1904ba9a90efec0b900dfcb551faf883
Fernando Lobos <fernando.lobos@latam.com>
2021-04-29 21:34:20 -0400

    style

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { HomePage } from './home';
+import { AddIngredientPage } from './ingredients';
+import { RecipeSearchPage } from './recipes';
+import { ShoppingListPage } from './shopping-list';
 
-const routes = [];
+const routes = [{
+    path: '/',
+    Component: HomePage,
+    exact: true,
+}, {
+    path: '/recipes',
+    Component: RecipeSearchPage,
+}, {
+    path: '/add-ingredient',
+    Component: AddIngredientPage,
+}, {
+    path: '/shopping-list',
+    Component: ShoppingListPage,
+}];
 
-export const Router = () => (
-    <Route>
+export const Routes = () => (
+    <Router>
       <Switch>
           {routes.map((route, index) =>(
               <Route key={index} path={route.path} exact={route.exact}>
@@ -12,6 +29,6 @@ export const Router = () => (
               </Route>
           ))}
       </Switch>
-   </Route>
+   </Router>
 )
 
